57c65163c058eddf45027c465e6dc5b64b5c9f94
Kevin Gori <kgori@ebi.ac.uk>
2013-09-23 14:06:47 +0100

    syntax correction

--- stirling.py
+++ stirling.py
@@ -1,50 +1,59 @@
 #!/usr/bin/env python
 
+import math
+
+
 class Stirling(object):
-    """
-    Stirling numbers of the first and second kind,
-    as used in Ane et al. (2006)
-    """
+
+    """ Stirling numbers of the first and second kind, as used in Ane et al.
+    (2006) """
 
     def __init__(self):
         self.s1memo = dict()
         self.s2memo = dict()
 
-    def a(n, k):
+    def a(self, n, k):
 
-        if (n,k) in self.s1memo:
-            return self.s1memo[(n,k)]
-
-        if n == k:
-            return 1
-
-        if k > n:
-            return 0 
-
-        if (n > 0) and (k == 0):
-            return 0
-
-        answer = self.a(n-1, k-1) + (n-1)*self.a(n-1, k)
-        self.s1memo[(n,k)] = answer
-        return answer
-
-    def S(n, k):
-
-        if (n,k) in self.s2memo:
-            return self.s2memo[(n,k)]
+        if (n, k) in self.s1memo:
+            return self.s1memo[(n, k)]
 
         if n == k:
             return 1
 
         if k > n:
-            return 0 
-
-        if (n > 0) and (k == 0):
             return 0
 
-        if (n > 0) and (k == 1):
-            return 1        
+        if n > 0 and k == 0:
+            return 0
 
-        answer = self.S(n-1, k-1) + k*self.S(n-1, k)
-        self.s2memo[(n,k)] = answer
+        answer = self.a(n - 1, k - 1) + (n - 1) * self.a(n - 1, k)
+        self.s1memo[(n, k)] = answer
         return answer
+
+    def S(self, n, k):
+
+        if (n, k) in self.s2memo:
+            return self.s2memo[(n, k)]
+
+        if n == k:
+            return 1
+
+        if k > n:
+            return 0
+
+        if n > 0 and k == 0:
+            return 0
+
+        if n > 0 and k == 1:
+            return 1
+
+        answer = self.S(n - 1, k - 1) + k * self.S(n - 1, k)
+        self.s2memo[(n, k)] = answer
+        return answer
+
+    def A(self, n, x):
+
+        ans = 0
+        for k in range(1, n + 1):
+            ans += self.a(n, k) * x**k
+        return ans
